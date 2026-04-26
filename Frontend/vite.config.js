8440707f41cf3cfef26ba6a0cf1a9d184a9a0fbd
iqsanazhr <iqsan17azhar@gmail.com>
2026-04-26 22:20:48 +0700

    Update render.yaml to include Frontend & remove Vite base

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/H1D024009-Iqsan_Azhar_N-ResponsiKB-ShiftA/', // Path repository GitHub Pages
+  // Base URL dihapus untuk hosting di Render
   plugins: [
     tailwindcss(),
     react()
